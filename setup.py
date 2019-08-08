40e9a8bc3c2a3f60385eab5200fd251fd5349d22
Shiraz Butt <shiraz.b@icloud.com>
2019-08-08 16:46:07 +0100

    setup pypi package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pytorch_autograd_checkpointing',
+      version='0.1',
+      description='stuff',
+      url='https://github.com/shirazb/pytorch-autograd-checkpointing',
+      author='Shiraz Butt',
+      author_email='shiraz.b@icloud.com',
+      license='MIT',
+      packages=['pytorch_autograd_checkpointing'],
+      zip_safe=False) # ??

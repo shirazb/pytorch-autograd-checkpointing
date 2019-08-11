aa537c0d0f46ee726e512b4213c75bee1452d51f
Shiraz Butt <shiraz.b@icloud.com>
2019-08-11 16:20:42 +0100

    move to src/ package structure (which for some reason can't be installed with -e

--- setup.py
+++ setup.py
@@ -2,13 +2,19 @@ from setuptools import setup
 
 setup(
         name='pytorch_autograd_checkpointing',
-        version='0.1',
+        version='0.1.0',
         description='stuff',
         url='https://github.com/shirazb/pytorch-autograd-checkpointing',
         author='Shiraz Butt',
         author_email='shiraz.b@icloud.com',
         license='MIT',
+
+        package_dir={'pytorch_autograd_checkpointing': 'src'},
         packages=['pytorch_autograd_checkpointing'],
-        install_requires=['torch'],
+        
+        install_requires=['torch>=1.1.0'],
+        #setup_requires=['pytest-runner'],
+        #tests_require=['pytest'],
+
         zip_safe=False # ??
 )

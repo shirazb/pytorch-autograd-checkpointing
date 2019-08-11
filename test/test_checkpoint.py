d1664bd683e2edefc52c7a1036413d0ee235db16
Shiraz Butt <shiraz.b@icloud.com>
2019-08-11 17:20:29 +0100

    test grads the same with and without checkpointing

---
+++ test/test_checkpoint.py
@@ -0,0 +1,59 @@
+import pytest
+
+import torch
+import pytorch_autograd_checkpointing as c
+
+def calc_grad(run_function):
+    t = torch.randn(10, 10, requires_grad=True)
+
+    y = run_function(t).sum()
+    y.backward()
+
+    return t.grad
+
+def test_does_not_change_grad():
+    ## Calc grad using original model
+    
+    # For reproducability
+    torch.manual_seed(23)
+
+    original_model = torch.nn.Sequential(
+            torch.nn.Linear(10, 10),
+            torch.nn.Linear(10, 10),
+            torch.nn.Linear(10, 10),
+            torch.nn.Linear(10, 10),
+            torch.nn.Linear(10, 10),
+    )
+
+    grad_original = calc_grad(original_model)
+
+    ## Calc grad using checkpointed model
+
+    torch.manual_seed(23)
+
+    checkpointed_model = torch.nn.Sequential(
+            torch.nn.Linear(10, 10),
+            c.Drop(
+                torch.nn.Sequential(
+                    torch.nn.Linear(10, 10),
+                    c.Drop(
+                        torch.nn.Linear(10, 10)
+                    , 2),
+                    torch.nn.Linear(10, 10)
+                )
+            , 1),
+            torch.nn.Linear(10, 10)
+    )
+
+    grad_with_checkpointing = calc_grad(checkpointed_model)
+
+    ## Compare equal
+    assert (grad_with_checkpointing == grad_original).all(), (
+            'Grads not equal.\n'    
+            '    With:\n'
+            '      {}\n'
+            '    Without:\n'
+            '      {}'
+            .format(grad_with_checkpointing, grad_original)
+    )
+    

96b373875e8de6c4149ce5225ba10115c49c0666
Shiraz Butt <shiraz.b@icloud.com>
2019-08-18 19:25:15 +0100

    profiling requires inputs and upstream grads to be given

--- pytorch_autograd_checkpointing/CheckpointedSequential.py
+++ pytorch_autograd_checkpointing/CheckpointedSequential.py
@@ -15,15 +15,50 @@ class CheckpointedSequential():
             )
         self.has_profiled = False
 
-    def profile_sequence(self):
+    def profile_sequence(self, inputs, upstream_gradients):
+        """
+          Dimensions:
+
+          [N+1][N+1][M].
+          i: f_0 ... f_N   (size N+1)
+          j: b_1 ... b_N+1 (size N+1)
+          ==> Use regular indexes, both have size N+2, i is padded at end, 
+              j at beginning.
+
+          Boundaries:
+
+          - f_0 is input.
+              - Compute = 0
+              - Memory = size of given input
+          - f_N is output.
+              - Compute = layer N-1
+              - Memory = sizeof output of layer N-1
+          - b_1 is grad of input
+              - Compute = layer 0'
+              - Memory = size of output of layer 0'
+          - b_N+1 is grad output
+              - Compute = 0
+              - Memory = size of given upstream grad
+        """
         self.compute_costs = []
         self.memory_costs = []
         self.has_profiled = True
 
-    def solve_optimal_policy(self, M, profile_memory=True, profile_compute=True):
+    def solve_optimal_policy(
+            self,
+            M,
+            inputs=None, upstream_gradients=None, # if profile true, must be given or error
+            profile_memory=True, profile_compute=True
+    ):
         if not self.has_profiled and (profile_memory or profile_compute):
-            self.profile_sequence()
-        
+            if inputs is None or upstream_gradients is None:
+                raise TypeError('CheckpointedSequential.solve_optimal_policy():'
+                        ' If profiling, inputs and upstream gradients MUST be '
+                        ' provided.'
+                )
+            else:
+                self.profile_sequence(inputs, upstream_gradients)
+
         # TODO: Handle setting of uniform costs if no profiling
 
         # TODO: solve optimal policy
@@ -31,6 +66,7 @@ class CheckpointedSequential():
 
         return policy
 
-    def backprop_sequence(self, policy, inputs, upstream_gradient, callbacks={}):
+    # TODO: Callbacks
+    def backprop_sequence(self, policy, inputs, upstream_gradients, callbacks={}):
         pass
 

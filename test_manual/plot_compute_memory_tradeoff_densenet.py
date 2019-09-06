928a35dc1f54e4a389b3344bbaf67de4366dbf4f
Shiraz Butt <shiraz.b@icloud.com>
2019-09-06 20:46:30 +0100

    densenet C-M tradeoff plotter

---
+++ test_manual/plot_compute_memory_tradeoff_densenet.py
@@ -0,0 +1,79 @@
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+
+import pytorch_autograd_checkpointing as c
+import models.densenet as DenseNetFactory
+
+from testing_utils import *
+
+_DEFAULT_OUTFILE_PREFIX = 'results/'
+_DEFAULT_OUTFILE_NAME = 'compute_mem_tradeoff_densenet.png'
+
+def plot_compute_memory_tradeoff():
+    results = {}
+
+    models = [
+        (DenseNetFactory.densenet121(), 'DenseNet-121'),
+        (DenseNetFactory.densenet201(), 'DenseNet-201'),
+        (DenseNetFactory.densenet264(), 'DenseNet-264')
+    ]
+
+    for (model, model_name) in models:
+        # Set up results
+        dense_chkpter = mk_checkpointed_densenet(model)
+
+        batch_size = 32
+        bucket_size = int(3e6)
+        budget_leeway = 0.2
+        gpu_mem_capcity = int(7e9)
+
+        x = densenet_dummy_input(batch_size)
+        b = (torch.tensor(1.),)
+
+        _, C, _ = prof_and_solve_policy(
+                dense_chkpter,
+                x, b,
+                gpu_mem_capcity,
+                bucket_size,
+                budget_leeway,
+                log=True
+        )
+
+        # The x axis, up to the internal memory budget in increments of 20 buckets.
+        MS = np.arange(4, C.shape[2]-1, 20 * bucket_size, dtype=np.int16)
+
+        results[model_name] = { 'm': MS, 'c': C[0, -1, MS] }
+
+        print('Done {}'.format(model_name))
+
+
+    # Plot C-M graphs for each N.
+
+    cols = 3
+    rows = int(np.ceil(len(models) / cols))
+    fig, axes = plt.subplots(rows, cols, sharex=True, figsize=(cols*7, rows*7))
+
+    if rows == 1:
+        axes = np.expand_dims(axes, axis=0)
+
+    for i in range(rows):
+        for j in range(cols):
+            n = i * cols + j
+            model_name = models[n][1]
+
+            if n >= len(models):
+                break
+
+            axes[i, j].plot(results[model_name]['m'], results[model_name]['c'])
+            axes[i, j].set_xlabel('Memory Budget')
+            axes[i, j].set_ylabel('Simulated Computational Cost')
+            axes[i, j].set_title(model_name)
+
+    #fig.suptitle('Compute-Memory Trade-Off for Varying Number Layers, N')
+    
+    outfile_path = _DEFAULT_OUTFILE_PREFIX + _DEFAULT_OUTFILE_NAME
+    plt.savefig(outfile_path, bbox_inches='tight')
+
+if __name__ == "__main__":
+    plot_compute_memory_tradeoff()
--- test_manual/testing_utils.py
+++ test_manual/testing_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import pytorch_autograd_checkpointing as c
 
 _DIM = 100
 
@@ -26,3 +27,52 @@ class ThresholdedLinear(torch.nn.Module):
 
     def forward(self, x):
         return torch.nn.functional.relu(self.fc(x))
+
+class SumLayer(torch.nn.Module):
+    def forward(self, x):
+        return x.sum()
+
+def mk_checkpointed_densenet(
+        densenet,
+        loss=SumLayer()
+):
+    modules = [module for k, module in densenet._modules.items()][0]
+
+    assert isinstance(modules, torch.nn.Sequential), 'run_solver_densenet(): modules was not a torch.nn.Sequential'
+
+    modules.add_module('fake_loss', loss)
+    
+    return c.CheckpointedSequential(modules)
+
+def prof_and_solve_policy(
+        model_chkpter,
+        x, b,
+        M,
+        bucket_size=int(3e6),
+        budget_leeway=0.2,
+        log=False
+):
+    model_chkpter.profile_sequence(x, b)
+
+    memory_costs = model_chkpter.memory_costs
+    memory_costs = np.ceil(memory_costs / float(bucket_size)).astype(int)
+    compute_costs = model_chkpter.compute_costs 
+
+    if log:
+        print('Costs: (m,c)')
+        print(memory_costs)
+        print(compute_costs)
+    
+    budget = budget_bucketed_with_leeway(M, bucket_size, budget_leeway)
+
+    return model_chkpter.solve_optimal_policy(
+        budget, 
+        compute_costs=compute_costs, memory_costs=memory_costs,
+        profile_compute=False, profile_memory=False
+    )
+
+def budget_bucketed_with_leeway(budget, bucket_size, leeway):
+    return int(budget // bucket_size * (1 - leeway))
+
+def densenet_dummy_input(batch_size):
+    return torch.randn(batch_size, 3, 224, 224).fill_(1.0)

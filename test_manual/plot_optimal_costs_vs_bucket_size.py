9e832d8ae7b21273aec6fd7cf6a6d5a2abe89828
Shiraz Butt <shiraz.b@icloud.com>
2019-09-06 23:10:59 +0100

    plot optimal costs against bucket size

---
+++ test_manual/plot_optimal_costs_vs_bucket_size.py
@@ -0,0 +1,93 @@
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+
+import pytorch_autograd_checkpointing as c
+import models.densenet as DenseNetFactory
+
+from testing_utils import *
+
+_DEFAULT_OUTFILE_PREFIX = 'results/'
+_DEFAULT_OUTFILE_NAME = 'optimal_cost_vs_bucket_size.png'
+
+def plot_optimal_cost_against_bucket_size():
+    mb = int(1e6)
+    bucket_sizes = [
+        int(b * mb) for b in range(1, 64, 4)
+    ]
+
+    batch_size = 32
+    budget_leeway = 0.2
+    gpu_mem_capcity_bytes = int(6e9)
+
+    models = [
+        {
+            'name': 'DenseNet-121',
+            'chkpter': mk_checkpointed_densenet(DenseNetFactory.densenet121()),
+            'x': densenet_dummy_input(batch_size),
+            'b': (torch.tensor(1.),)
+        }
+    ]
+
+    results = {}
+
+    # For each model, find the optimal policy for each bucket size, and plot corresponding optimal costs.
+    for model in models:
+        name = model['name']
+        chkpter = model['chkpter']
+        x = model['x']
+        b = model['b']
+
+        # Init results for this model
+        results[name] = { 'b': [], 'c': [] }
+
+        # Use same profile results for each bucket size
+        chkpter.profile_sequence(x, b)
+
+        for bucket_size in bucket_sizes:
+            compute_costs, memory_costs = bucket_costs(chkpter, bucket_size, log=True)
+                
+            _, C, _ = solve_policy_using_costs(
+                    chkpter,
+                    compute_costs, memory_costs,
+                    gpu_mem_capcity_bytes,
+                    bucket_size,
+                    budget_leeway
+            )
+
+            results[name]['b'].append(bucket_size)
+            results[name]['c'].append(C[0, -1, -1])
+
+        print('Done {}'.format(name))
+
+    # Plot optimal cost vs bucket size graph for each model.
+
+    cols = 2
+    rows = int(np.ceil(len(models) / cols))
+    fig, axes = plt.subplots(rows, cols, sharex=True, figsize=(cols*7, rows*7))
+
+    if rows == 1:
+        axes = np.expand_dims(axes, axis=0)
+
+    for i in range(rows):
+        for j in range(cols):
+            n = i * cols + j
+
+            if n >= len(models):
+                break
+
+            model = models[n]
+            name = model[name]
+
+            axes[i, j].plot(int(results[name]['b'] // mb), results[name]['c'])
+            axes[i, j].set_xlabel('Bucket Size, MB')
+            axes[i, j].set_ylabel('Optimal (Simulated) Computational Cost, ms')
+            axes[i, j].set_title(name)
+
+    #fig.suptitle('Compute-Memory Trade-Off for Varying Number Layers, N')
+    
+    outfile_path = _DEFAULT_OUTFILE_PREFIX + _DEFAULT_OUTFILE_NAME
+    plt.savefig(outfile_path, bbox_inches='tight')
+
+if __name__ == "__main__":
+    plot_optimal_cost_against_bucket_size()
--- test_manual/testing_utils.py
+++ test_manual/testing_utils.py
@@ -44,16 +44,11 @@ def mk_checkpointed_densenet(
     
     return c.CheckpointedSequential(modules)
 
-def prof_and_solve_policy(
+def bucket_costs(
         model_chkpter,
-        x, b,
-        M,
-        bucket_size=int(3e6),
-        budget_leeway=0.2,
+        bucket_size,
         log=False
 ):
-    model_chkpter.profile_sequence(x, b)
-
     memory_costs = model_chkpter.memory_costs
     memory_costs = np.ceil(memory_costs / float(bucket_size)).astype(int)
     compute_costs = model_chkpter.compute_costs 
@@ -63,6 +58,30 @@ def prof_and_solve_policy(
         print(memory_costs)
         print(compute_costs)
     
+    return compute_costs, memory_costs
+
+def prof_model(
+        model_chkpter,
+        x, b,
+        bucket_size=int(3e6),
+        log=False
+):
+    model_chkpter.profile_sequence(x, b)
+    
+    return bucket_costs(
+        model_chkpter,
+        bucket_size,
+        log
+    )
+
+def solve_policy_using_costs(
+        model_chkpter,
+        compute_costs,
+        memory_costs,
+        M,
+        bucket_size,
+        budget_leeway=0.2
+):
     budget = budget_bucketed_with_leeway(M, bucket_size, budget_leeway)
 
     return model_chkpter.solve_optimal_policy(
@@ -71,8 +90,25 @@ def prof_and_solve_policy(
         profile_compute=False, profile_memory=False
     )
 
+def prof_and_solve_policy(
+        model_chkpter,
+        x, b,
+        M,
+        bucket_size,
+        budget_leeway=0.2,
+        log=False
+):
+    compute_costs, memory_costs = prof_model(model_chkpter, x, b, bucket_size, log)
+    
+    return solve_policy_using_costs(
+        model_chkpter,
+        compute_costs, memory_costs,
+        M,
+        bucket_size, budget_leeway
+    )
+
 def budget_bucketed_with_leeway(budget, bucket_size, leeway):
-    return int(budget // bucket_size * (1 - leeway))
+    return int((budget // bucket_size) * (1 - leeway))
 
 def densenet_dummy_input(batch_size):
     return torch.randn(batch_size, 3, 224, 224).fill_(1.0)
